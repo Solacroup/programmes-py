f06d870bb61246058e709e41416a827401150e02
Morgane <72966814+Solacroup@users.noreply.github.com>
2021-10-11 20:20:45 +0200

    Create produit.py

---
+++ produit.py
@@ -0,0 +1,7 @@
+from cs50 import get_int
+
+# recueillir les entiers
+a = get_int("entier 1 : ")
+b = get_int("entier 2 : ")
+# faire le produit des entiers et afficher le résultat
+print(f"le produit des 2 entiers vaut : {a * b} \n")
